3d2246545ab87b45f8c83e119755809afe09bccc
dpes44 <83898800+dpes44@users.noreply.github.com>
2025-05-29 17:06:56 +0545

    Update config.js

--- chart/js/config.js
+++ chart/js/config.js
@@ -1,14 +1,33 @@
+// export default class Config {
+//   static apiKey = "zeYfVRNaPP_E-fQxxHelQ";
+//   static apiEndpoint = "https://mcgqs.vitalstats.app/api/v1/graphql";
+//   static visitorReferralSource = visitorFromOp;
+//   static jobStatusType = jobStatusTypeFromOp;
+//   static jobStatusTypeColor = jobStatusColorFromOp;
+//   static jobStatusTypeCondtion = jobStatusConditionFromOp;
+//   static get wsUrl() {
+//     return `wss://mcgqs.vitalstats.app/api/v1/graphql?apiKey=${this.apiKey}`;
+//   }
+
+//   static get restUrl() {
+//     return `https://mcgqs.vitalstats.app/api/v1/graphql?apiKey=${this.apiKey}`;
+//   }
+// }
+
+
+const allJobStatusAndColorAndConditions = [
+  { type: "Pending Inspection", color: "#10b981", condition: "Pending Inspection" },
+  { type: "Completed", color: "#f59e0b", condition: "Report Sent" },
+  { type: "Waiting On Info", color: "#ef4444", condition: "Waiting On Info" },
+];
 export default class Config {
   static apiKey = "zeYfVRNaPP_E-fQxxHelQ";
   static apiEndpoint = "https://mcgqs.vitalstats.app/api/v1/graphql";
-  static visitorReferralSource = visitorFromOp;
-  static jobStatusType = jobStatusTypeFromOp;
-  static jobStatusTypeColor = jobStatusColorFromOp;
-  static jobStatusTypeCondtion = jobStatusConditionFromOp;
+  static visitorReferralSource = 'Gallagher Melbourne';
+  static jobStatuses = allJobStatusAndColorAndConditions;
   static get wsUrl() {
     return `wss://mcgqs.vitalstats.app/api/v1/graphql?apiKey=${this.apiKey}`;
   }
-
   static get restUrl() {
     return `https://mcgqs.vitalstats.app/api/v1/graphql?apiKey=${this.apiKey}`;
   }
